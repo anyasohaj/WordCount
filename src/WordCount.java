ab7b1e36e82a2fca6b9d8dd3e14b4a30d794fbbc
Agnes Balazs <anyasohaj@gmail.com>
2018-01-17 18:25:32 +0200

    Initial commit

---
+++ src/WordCount.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.io.File;
+
+public class WordCount {
+    public static void main(String [] args)throws Exception{
+    File file = new File("EgyMagyarNabob1.txt");
+    Scanner scanner = new Scanner(file);
+    int words = 0;
+    while (scanner.hasNextLine()){
+        String line = scanner.nextLine();
+        words += line.split(" ").length;
+    }
+    System.out.println(words);
+    }
+}
+
